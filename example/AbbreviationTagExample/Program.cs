28a026de2c7b0cf8a98c778e9b4a393aa28710f8
Yevhen Cherkes <ycherkes@outlook.com>
2025-07-24 09:51:39 +0200

    Initial commit

---
+++ example/AbbreviationTagExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using HashTagExample;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+await using var connection = new SqliteConnection("DataSource=:memory:");
+connection.Open();
+
+var options = new DbContextOptionsBuilder<TestDbContext>()
+    .EnableSensitiveDataLogging()
+    .LogTo(Console.WriteLine)
+    .UseSqlite(connection)
+    .Options;
+
+var context = new TestDbContext(options);
+context.Database.EnsureCreated();
+
+var products = await context.Products
+    .TagWithCallSiteAbbreviation()
+    .ToArrayAsync();
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.Name);
+}
